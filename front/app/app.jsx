8886d249faddfae5e5f1442576f394f7770f5864
polinakhadorchanka <polina_98_21@mail.ru>
2020-03-01 21:12:47 +0300

    0103

--- front/app/app.jsx
+++
@@ -1,8 +0,0 @@
-let ReactDOM = require('react-dom');
-let React = require('react');
-let Tabs = require('./components/Tabs.jsx');
-
-ReactDOM.render(
-    <Tabs />,
-    document.getElementById("container")
-);
